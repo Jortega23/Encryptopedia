bbd4f28b306139074b80965ed65f7a6866ba4f83
Jortega23 <jortegs23@gmail.com>
2020-08-05 07:31:34 -0700

    WIP 8/5/2020
    Search component worked on. Added useSate and some styling

--- encryptopedia/src/components/Search.js
+++ encryptopedia/src/components/Search.js
@@ -1,11 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 const Search = () => {
+  const [text, setText] = useState('')
+
+  const onSubmit = (e) => {
+    e.preventDefault()
+    console.log(text);
+  }
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  }
   return (
     <div>
-      <form>
+      <form onSubmit={onSubmit} className='form'>
         <div className='search-bar'>
-          <input type='search' placeholder='Search Crypto' />
+          <input type='search' name='text' placeholder='Search Crypto' onChange={onChange}/>
           <input type='submit' value='Seach'/>
         </div>
       </form>
